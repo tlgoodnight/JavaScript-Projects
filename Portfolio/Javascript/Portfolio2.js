ddaa2ee30a472470d62bf971fcdc5c73fe891ec4
tlgoodnight <tlgoodnight@me.com>
2021-12-13 07:27:35 -0800

    Portfolio Update

--- Portfolio/Javascript/Portfolio2.js
+++ Portfolio/Javascript/Portfolio2.js
@@ -1,45 +1,45 @@
-
+// These functions open and close the contact form
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
-
-function validateForm() {
-  let x = document.forms["myForm"]["Name"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-  let y = document.forms["myForm"]["Email"].value;
-  if (y == "") {
-    alert("Email must be filled out");
-    return false;
-  }
+  document.getElementById("myForm").style.display = "block";
 }
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
-      // Store hash
-      var hash = this.hash;
+// This function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+// This function changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+// This function changes the slide when the dots are clicked
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
+  var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
+  if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+  if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
+  }
+  slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
+  dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
+}
+
+// This code will create close the contact form when the user clicks off of it
+// The first step is to add an event listener for any clicks on the website
+document.addEventListener("click", function(event){
+  // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
+  if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+      closeForm()
+  }
+}, false )
