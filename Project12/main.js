b318602a31f7c2b6d1c47febfbdaf99255a2c8ce
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2021-12-14 06:40:53 -0800

    Delete Project12 directory

--- Project12/main.js
+++
@@ -1,9 +0,0 @@
-function count_To_Ten() {
-    var Digit = "";
-    var X = 1;
-    while (X < 11) {
-        Digit += "<br>" + X;
-        X++;
-    }
-    document.getElementById("Counting_to_Ten").innerHTML = Digit;
-}
